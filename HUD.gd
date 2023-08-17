968952414c1b5c5a6cfcae6845cba3f18f2769be
Joa <ja.cutillas@iesdoctorbalmis.com>
2023-08-17 23:09:33 +0200

    debug como genios

--- HUD.gd
+++ HUD.gd
@@ -15,7 +15,7 @@ func show_game_over():
 	$Message.text = "Dodge the\nCreeps!"
 	$Message.show()
 	# Make a one-shot timer and wait for it to finish.
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.3).timeout
 	$StartButton.show()
 	
 func update_score(score):
